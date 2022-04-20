39132cefeabe7a28d190dbd20438e25ee5335d46
Kevin <hellokevinvogel@gmail.com>
2022-04-21 01:15:48 +0200

    chore(balance): add withdraw funds

---
+++ pkg/bank_balance/routes/withdraw_funds.go
@@ -0,0 +1,30 @@
+package routes
+
+import (
+	"context"
+
+	"github.com/gofiber/fiber/v2"
+	pb "github.com/hellokvn/bank-api-gateway/pkg/bank_balance/pb"
+)
+
+type WithdrawFundsRequestBody struct {
+	Amount int32 `json:"amount"`
+}
+
+func WithdrawFunds(ctx *fiber.Ctx, c pb.BankBalanceCommandServiceClient) error {
+	body := WithdrawFundsRequestBody{}
+
+	if err := ctx.BodyParser(&body); err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, err.Error())
+	}
+	res, err := c.WithdrawFunds(context.Background(), &pb.WithdrawFundsRequest{
+		Id:     ctx.Params("id"),
+		Amount: body.Amount,
+	})
+
+	if err != nil {
+		return fiber.NewError(fiber.StatusBadGateway, err.Error())
+	}
+
+	return ctx.Status(int(res.Status)).JSON(&res)
+}
