548670306d9706ea7801a43ed73af8143407d93b
Kevin <hellokevinvogel@gmail.com>
2022-04-21 00:11:38 +0200

    chore(account): add open account, update proto

--- pkg/bank_account/routes.go
+++ pkg/bank_account/routes.go
@@ -7,15 +7,22 @@ import (
 )
 
 func RegisterRoutes(app *fiber.App, c config.Config) {
-	svc := &ServiceClient{
-		Client: InitServiceClient(&c),
-	}
+	svc := InitServiceClient(&c)
 
 	r := app.Group("/bank-account")
 	r.Get("/find-one/:id", svc.FindAccount)
-	r.Get("/find/:page", svc.FindAccount)
+	r.Get("/find/:page", svc.FindAllAccounts)
+	r.Post("/", svc.OpenAccount)
 }
 
 func (svc *ServiceClient) FindAccount(ctx *fiber.Ctx) error {
-	return routes.FindAccount(ctx, svc.Client)
+	return routes.FindAccount(ctx, svc.QueryClient)
+}
+
+func (svc *ServiceClient) FindAllAccounts(ctx *fiber.Ctx) error {
+	return routes.FindAllAccounts(ctx, svc.QueryClient)
+}
+
+func (svc *ServiceClient) OpenAccount(ctx *fiber.Ctx) error {
+	return routes.OpenAccount(ctx, svc.CommandClient)
 }
---
+++ pkg/bank_account/routes/open_account.go
@@ -0,0 +1,43 @@
+package routes
+
+import (
+	"context"
+
+	"github.com/gofiber/fiber/v2"
+	pb "github.com/hellokvn/bank-api-gateway/pkg/bank_account/pb"
+)
+
+type AccountType string
+
+const (
+	Savings AccountType = "SAVINGS"
+	Current             = "CURRENT"
+)
+
+type OpenAccountRequestBody struct {
+	FirstName      string      `json:"firstName"`
+	LastName       string      `json:"lastName"`
+	OpeningBalance int32       `json:"openingBalance"`
+	Type           AccountType `json:"type"`
+}
+
+func OpenAccount(ctx *fiber.Ctx, c pb.BankAccountCommandServiceClient) error {
+	body := OpenAccountRequestBody{}
+
+	if err := ctx.BodyParser(&body); err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, err.Error())
+	}
+
+	res, err := c.OpenAccount(context.Background(), &pb.OpenAccountRequest{
+		FirstName:      body.FirstName,
+		LastName:       body.LastName,
+		Type:           body.Type,
+		OpeningBalance: body.OpeningBalance,
+	})
+
+	if err != nil {
+		return fiber.NewError(fiber.StatusBadGateway, err.Error())
+	}
+
+	return ctx.Status(int(res.Status)).JSON(&res)
+}
